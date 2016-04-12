f9fe4ce62e7bf3da927a32407944c1118f18bf5f
mfcastro <mfcastro50@gmail.com>
2016-04-12 18:04:43 -0500

    Started Project file in Visual Studio

---
+++ RetailAdvertisingTool/RetailAdvertisingTool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RetailAdvertisingTool.Startup))]
+namespace RetailAdvertisingTool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
